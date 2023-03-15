5e477a4433d6d155bbb80c8a1cf5476285190674
Chinika charles <chinikacharles@chinikas-mbp.home>
2023-03-15 19:22:07 +0000

    POST function working - can create a new pet and view it on the webpage

--- src/components/PetsForm.js
+++ src/components/PetsForm.js
@@ -1,6 +1,6 @@
 import {useState} from "react"
 
-const PetsForm = ({pets}) => {
+const PetsForm = ({pets, onSubmit}) => {
     
     const [newPets, setNewPets] = useState({
         name: "",
@@ -15,15 +15,60 @@ const PetsForm = ({pets}) => {
         </option>
     })
 
+    const handleChange = (event) => {
+        const propertyName = event.target.name;
+        const copiedPet = {...newPets};
+        copiedPet[propertyName] = event.target.value;
+        setNewPets(copiedPet);
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(newPets);
+    }
+
     return(
-        <form id="pets-form">
+        <form id="pets-form" onSubmit={handleFormSubmit}>
             <h3>Add a new Pet!</h3>
-            <label htmlFor="pets-name">Pet Name:</label>
+
+            <label htmlFor="pet-name">Pet Name:</label>
             <input 
-            id="pets-name"
+            id="pet-name"
             name="name"
             type="text"
+            placeholder="Enter Pet Name"
+            onChange={handleChange}
              />
+             
+             <label htmlFor="pet-age">Pet Age (in years):</label>
+             <input
+                id="pet-age"
+                name="age"
+                type="number"
+                min={0}
+                max={100}
+                onChange={handleChange}
+            />
+
+            <label htmlFor="pet-type">Pet Type:</label>
+            <input 
+                id="pet-type" 
+                name="type"
+                type="text"
+                placeholder="Enter Pet Type"
+                onChange={handleChange}
+            />
+            
+            <label htmlFor="pet-breed">Pet Breed:</label>
+            <input
+                id="pet-breed" 
+                name="breed"
+                type="text"
+                placeholder="Enter Pet Breed"
+                onChange={handleChange}
+            />
+
+            <input type="submit" value="Add Pet"/>
 
             </form>
     )
